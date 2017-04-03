f829390ae6c2bc03366ca2ebb221df6c874f1203
Shmueli Englard <shmueli.yosef@englard.net>
2017-04-03 16:03:34 -0400

    Added Checkout-Branch cmdlet

---
+++ Checkout-Branch.ps1
@@ -0,0 +1,64 @@
+function Checkout-Branch
+{
+	<#
+		.Synopsis
+		Switch branches
+
+		.Description
+		Updates files in the working tree to match the version in the specified tree.
+
+		.Parameter Repository
+		Path to the git repository. Can be relative or absolute. If not specified defaults to the current directory
+
+		.Parameter Branch
+		Branch to checkout
+
+		.Parameter CreateNew
+		Create a new branch
+
+		.Parameter Force
+		When switching branches, proceed even if the working tree differs from HEAD. This is used to throw away local changes.
+
+		.Parameter Track
+		When creating a new branch, set up "upstream" configuration.
+
+		.Link
+		https://git-scm.com/docs/git-checkout
+	#>
+	[CmdletBinding()]
+	param(
+		[Parameter(Mandatory=$False,Position=1,ValueFromPipeline=$True)]
+		[string]$Repository = ".\",
+		[Parameter(Mandatory=$True)]
+		[string]$Branch,
+		[Alias("b")]
+		[switch]$CreateNew = $False,
+		[Alias("f")]
+		[switch]$Force = $False,
+		[Alias("t")]
+		[switch]$Track = $False
+	)
+	process
+	{
+		$Repository = Resolve-Path -Path $Repository
+		Write-Verbose "Checking out branch $Branch in $Repository"
+		$ExtendedCLI = "";
+		if ($CreateNew)
+		{
+			$ExtendedCLI += " -b"
+		}
+		if ($Force)
+		{
+			$ExtendedCLI += " -f"
+		}
+		if ($Track)
+		{
+			$ExtendedCLI += " --track"
+		}
+		$Output = (& "git -C $Repository checkout$ExtendedCLI $Branch") 2>&1
+		if ($Output.GetType().Name -eq "ErrorRecord")
+		{
+			Write-Error -Message ($Output.Exception.Message) -CategoryActivity ($Output.Exception.Message.SubString(0, $Output.Exception.Message.IndexOf(":"))) -ErrorId $LASTEXITCODE
+		}
+	}
+}
--- ObjectiveGit.psm1
+++ ObjectiveGit.psm1
@@ -4,9 +4,11 @@ $moduleRoot = Split-Path -Path $MyInvocation.MyCommand.Path -Parent
 . (Resolve-Path "$moduleRoot\Pull-SvnRepository.ps1")
 . (Resolve-Path "$moduleRoot\Stash-Changes.ps1")
 . (Resolve-Path "$moduleRoot\Restore-Changes.ps1")
+. (Resolve-Path "$moduleRoot\Checkout-Branch.ps1")
 
 Export-ModuleMember -Function Get-Git-Status
 Export-ModuleMember -Function Rebase-SvnRepository
 Export-ModuleMember -Function Pull-SvnRepository
 Export-ModuleMember -Function Stash-Changes
 Export-ModuleMember -Function Restore-Changes
+Export-ModuleMember -Function Checkout-Branch
