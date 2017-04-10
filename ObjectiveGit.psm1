4ca7cffb9c09ce439860a22b6ff71e263b8a1712
Shmueli Englard <shmueli.yosef@englard.net>
2017-04-10 14:31:38 -0400

    Corrected file name

--- ObjectiveGit.psm1
+++ ObjectiveGit.psm1
@@ -7,7 +7,7 @@ $moduleRoot = Split-Path -Path $MyInvocation.MyCommand.Path -Parent
 . (Resolve-Path "$moduleRoot\Checkout-Branch.ps1")
 . (Resolve-Path "$moduleRoot\Restore-Items.ps1")
 
-Export-ModuleMember -Function Get-Git-Status
+Export-ModuleMember -Function Get-RepositoryStatus
 Export-ModuleMember -Function Rebase-SvnRepository
 Export-ModuleMember -Function Pull-SvnRepository
 Export-ModuleMember -Function Stash-Changes
