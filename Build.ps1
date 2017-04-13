00c4765bae98384ca6651ce2e2271c30f8330a89
Shmueli Englard <shmueli.yosef@englard.net>
2017-04-13 17:07:20 -0400

    Update to version 1.5

--- Build.ps1
+++ Build.ps1
@@ -1,3 +1,3 @@
 vs2017
 $SolutionDir = Get-Location
-msbuild ObjectiveGit.pssproj /m /p:SolutionDir=$SolutionDir /p:Tags=Git%2cSvn /p:LicenseUri=https%3a%2f%2fgithub.com%2fSamuelEnglard%2fObjectiveGit%2fblob%2fmaster%2fLICENSE.txt /p:ProjectUri=https%3a%2f%2fgithub.com%2fSamuelEnglard%2fObjectiveGit /p:ReleaseNotes=Initial%20Publish
+msbuild ObjectiveGit.pssproj /m /p:SolutionDir=$SolutionDir /p:Tags=Git%2cSvn /p:LicenseUri=https%3a%2f%2fgithub.com%2fSamuelEnglard%2fObjectiveGit%2fblob%2fmaster%2fLICENSE.txt /p:ProjectUri=https%3a%2f%2fgithub.com%2fSamuelEnglard%2fObjectiveGit /p:ReleaseNotes=Renamed%20commands%20and%20bug%20fixes
